a060df93817b573dcf4108de6fe8589e6d855381
ali beigi <alighb30@gmail.com>
2023-08-01 21:21:20 +0330

    check flower app

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,277 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en_US': Locales.en_US,
+    'fa_IR': Locales.fa_IR,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const login_email = 'login_email';
+  static const login_passWord = 'login_passWord';
+  static const login_remember_me = 'login_remember_me';
+  static const login_login_button = 'login_login_button';
+  static const login_sign_up_button = 'login_sign_up_button';
+  static const login_title = 'login_title';
+  static const register_title = 'register_title';
+  static const register_first_name = 'register_first_name';
+  static const register_last_name = 'register_last_name';
+  static const register_email = 'register_email';
+  static const register_password = 'register_password';
+  static const register_confirmPassword = 'register_confirmPassword';
+  static const register_vendor = 'register_vendor';
+  static const register_customer = 'register_customer';
+  static const register_register_button = 'register_register_button';
+  static const vendor_page_title = 'vendor_page_title';
+  static const vendor_page_refresh = 'vendor_page_refresh';
+  static const vendor_page_home = 'vendor_page_home';
+  static const vendor_page_add_flower = 'vendor_page_add_flower';
+  static const vendor_page_search = 'vendor_page_search';
+  static const vendor_page_history = 'vendor_page_history';
+  static const vendor_page_profile = 'vendor_page_profile';
+  static const vendor_page_english_btn = 'vendor_page_english_btn';
+  static const vendor_page_farsi_btn = 'vendor_page_farsi_btn';
+  static const vendor_page_about = 'vendor_page_about';
+  static const vendor_home_item_name = 'vendor_home_item_name';
+  static const vendor_home_item_description = 'vendor_home_item_description';
+  static const vendor_home_item_color = 'vendor_home_item_color';
+  static const vendor_home_item_delete_btn = 'vendor_home_item_delete_btn';
+  static const vendor_home_item_price = 'vendor_home_item_price';
+  static const vendor_home_item_count = 'vendor_home_item_count';
+  static const vendor_home_add_flower_name = 'vendor_home_add_flower_name';
+  static const vendor_home_add_flower_description =
+      'vendor_home_add_flower_description';
+  static const vendor_home_add_flower_image = 'vendor_home_add_flower_image';
+  static const vendor_home_add_flower_price = 'vendor_home_add_flower_price';
+  static const vendor_home_add_flower_count = 'vendor_home_add_flower_count';
+  static const vendor_home_add_flower_color = 'vendor_home_add_flower_color';
+  static const vendor_home_add_flower_category =
+      'vendor_home_add_flower_category';
+  static const vendor_home_add_flower_add_btn =
+      'vendor_home_add_flower_add_btn';
+  static const home_search_search = 'home_search_search';
+  static const home_search_search_filter = 'home_search_search_filter';
+  static const home_search_search_color = 'home_search_search_color';
+  static const home_search_search_price = 'home_search_search_price';
+  static const home_search_search_ok_btn = 'home_search_search_ok_btn';
+  static const home_search_search_clear_btn = 'home_search_search_clear_btn';
+  static const vendor_home_history_flower_name =
+      'vendor_home_history_flower_name';
+  static const vendor_home_history_customer_name =
+      'vendor_home_history_customer_name';
+  static const vendor_home_history_bought_date =
+      'vendor_home_history_bought_date';
+  static const vendor_home_history_price = 'vendor_home_history_price';
+  static const vendor_home_history_count_bought =
+      'vendor_home_history_count_bought';
+  static const profile_first_name = 'profile_first_name';
+  static const profile_last_name = 'profile_last_name';
+  static const profile_email = 'profile_email';
+  static const profile_sign_out_btn = 'profile_sign_out_btn';
+  static const customer_page_title = 'customer_page_title';
+  static const customer_page_refresh = 'customer_page_refresh';
+  static const customer_page_home = 'customer_page_home';
+  static const customer_page_cart = 'customer_page_cart';
+  static const customer_page_search = 'customer_page_search';
+  static const customer_page_history = 'customer_page_history';
+  static const customer_page_profile = 'customer_page_profile';
+  static const customer_page_english_btn = 'customer_page_english_btn';
+  static const customer_page_farsi_btn = 'customer_page_farsi_btn';
+  static const customer_page_about = 'customer_page_about';
+  static const customer_home_item_name = 'customer_home_item_name';
+  static const customer_home_item_description =
+      'customer_home_item_description';
+  static const customer_home_item_vendor_name =
+      'customer_home_item_vendor_name';
+  static const customer_home_item_color = 'customer_home_item_color';
+  static const customer_home_item_add_cart_btn =
+      'customer_home_item_add_cart_btn';
+  static const customer_home_item_price = 'customer_home_item_price';
+  static const customer_home_item_count = 'customer_home_item_count';
+  static const customer_home_item_out_of_stock =
+      'customer_home_item_out_of_stock';
+  static const customer_home_item_count_buy = 'customer_home_item_count_buy';
+  static const customer_home_item_total_price =
+      'customer_home_item_total_price';
+  static const customer_home_item_purchase = 'customer_home_item_purchase';
+  static const customer_home_history_flower_name =
+      'customer_home_history_flower_name';
+  static const customer_home_history_vendor_name =
+      'customer_home_history_vendor_name';
+  static const customer_home_history_bought_date =
+      'customer_home_history_bought_date';
+  static const customer_home_history_price = 'customer_home_history_price';
+  static const customer_home_history_count_bought =
+      'customer_home_history_count_bought';
+}
+
+class Locales {
+  static const en_US = {
+    'login_email': 'Email',
+    'login_passWord': 'Password',
+    'login_remember_me': 'Remember Me',
+    'login_login_button': 'Login',
+    'login_sign_up_button': 'Sign Up',
+    'login_title': 'Login Page',
+    'register_title': 'Register Page',
+    'register_first_name': 'First Name',
+    'register_last_name': 'Last Name',
+    'register_email': 'Email',
+    'register_password': 'Password',
+    'register_confirmPassword': 'Confirm Password',
+    'register_vendor': 'Vendor',
+    'register_customer': 'Customer',
+    'register_register_button': 'Register',
+    'vendor_page_title': 'Vendor Home Page',
+    'vendor_page_refresh': 'Refresh',
+    'vendor_page_home': 'Home',
+    'vendor_page_add_flower': 'Add Flowers',
+    'vendor_page_search': 'Search',
+    'vendor_page_history': 'History',
+    'vendor_page_profile': 'Profile',
+    'vendor_page_english_btn': 'English',
+    'vendor_page_farsi_btn': 'فارسی',
+    'vendor_page_about': 'about',
+    'vendor_home_item_name': 'Name: ',
+    'vendor_home_item_description': 'Description: ',
+    'vendor_home_item_color': 'Color: ',
+    'vendor_home_item_delete_btn': 'Delete',
+    'vendor_home_item_price': 'Price: ',
+    'vendor_home_item_count': 'Count: ',
+    'vendor_home_add_flower_name': 'Flower Name',
+    'vendor_home_add_flower_description': 'Flower Description',
+    'vendor_home_add_flower_image': 'Add Flower Image',
+    'vendor_home_add_flower_price': 'Flower Price',
+    'vendor_home_add_flower_count': 'Flower Count',
+    'vendor_home_add_flower_color': 'Add Flower Color',
+    'vendor_home_add_flower_category': 'Add a Category',
+    'vendor_home_add_flower_add_btn': 'Add Flower',
+    'home_search_search': 'Search Or For Filter Press Search Icon',
+    'home_search_search_filter': 'Search Filter',
+    'home_search_search_color': 'Color Filter',
+    'home_search_search_price': 'Price Range Filter',
+    'home_search_search_ok_btn': 'ok',
+    'home_search_search_clear_btn': 'Clear Filter',
+    'vendor_home_history_flower_name': 'Flower Name: ',
+    'vendor_home_history_customer_name': 'Customer Name: ',
+    'vendor_home_history_bought_date': 'Bought Date: ',
+    'vendor_home_history_price': 'Price: ',
+    'vendor_home_history_count_bought': 'Count Bought: ',
+    'profile_first_name': 'First Name: ',
+    'profile_last_name': 'Last Name: ',
+    'profile_email': 'Email: ',
+    'profile_sign_out_btn': 'Sign Out: ',
+    'customer_page_title': 'Customer Home Page',
+    'customer_page_refresh': 'Refresh',
+    'customer_page_home': 'Home',
+    'customer_page_cart': 'Cart',
+    'customer_page_search': 'Search',
+    'customer_page_history': 'History',
+    'customer_page_profile': 'Profile',
+    'customer_page_english_btn': 'English',
+    'customer_page_farsi_btn': 'فارسی',
+    'customer_page_about': 'about',
+    'customer_home_item_name': 'Name: ',
+    'customer_home_item_description': 'Description: ',
+    'customer_home_item_vendor_name': 'Vendor Name: ',
+    'customer_home_item_color': 'Color: ',
+    'customer_home_item_add_cart_btn': 'Add to cart',
+    'customer_home_item_price': 'Price: ',
+    'customer_home_item_count': 'Count: ',
+    'customer_home_item_out_of_stock': 'Out of Stock',
+    'customer_home_item_count_buy': 'Count Buy',
+    'customer_home_item_total_price': 'Total Price: ',
+    'customer_home_item_purchase': 'Purchase',
+    'customer_home_history_flower_name': 'Flower Name: ',
+    'customer_home_history_vendor_name': 'Vendor Name: ',
+    'customer_home_history_bought_date': 'Bought Date: ',
+    'customer_home_history_price': 'Price: ',
+    'customer_home_history_count_bought': 'Count Bought: ',
+  };
+  static const fa_IR = {
+    'login_email': 'ایمیل',
+    'login_passWord': 'پسورد',
+    'login_remember_me': 'مرا به خاطر بسپار',
+    'login_login_button': 'ورود',
+    'login_sign_up_button': 'رجیستر',
+    'login_title': 'صفحه ورود',
+    'register_title': 'صفحه ثبت نام',
+    'register_first_name': 'نام',
+    'register_last_name': 'نام خانوادگی',
+    'register_email': 'ایمیل',
+    'register_password': 'رمز عبور',
+    'register_confirmPassword': 'تکرار رمز عبور ',
+    'register_vendor': 'فروشنده',
+    'register_customer': 'خریداد',
+    'register_register_button': 'ثبت نام',
+    'vendor_page_title': 'صفحه خانه فروشنده',
+    'vendor_page_refresh': 'بارگذاری مجدد',
+    'vendor_page_home': 'خانه',
+    'vendor_page_add_flower': 'افزودن گل',
+    'vendor_page_search': 'جستوجو',
+    'vendor_page_history': 'تاریخچه',
+    'vendor_page_profile': 'مشخصات',
+    'vendor_page_english_btn': 'English',
+    'vendor_page_farsi_btn': 'فارسی',
+    'vendor_page_about': 'در باره ما',
+    'vendor_home_item_name': 'نام: ',
+    'vendor_home_item_description': 'شرح: ',
+    'vendor_home_item_color': 'رنگ: ',
+    'vendor_home_item_delete_btn': 'حذف',
+    'vendor_home_item_price': 'قیمت: ',
+    'vendor_home_item_count': 'تعداد: ',
+    'vendor_home_add_flower_name': 'نام گل',
+    'vendor_home_add_flower_description': 'شرح گل',
+    'vendor_home_add_flower_image': 'عکس کل',
+    'vendor_home_add_flower_price': 'قیمت گل',
+    'vendor_home_add_flower_count': 'تعداد گل',
+    'vendor_home_add_flower_color': 'رنگ گل',
+    'vendor_home_add_flower_category': 'دسته بندی گل',
+    'vendor_home_add_flower_add_btn': 'افزودن گل',
+    'home_search_search': 'جستجو یا برای فیلتر نماد جستجو را فشار دهید',
+    'home_search_search_filter': 'فیلتر جستجو',
+    'home_search_search_color': 'فیلتر رنگ',
+    'home_search_search_price': 'فیلتر محدوده قیمت',
+    'home_search_search_ok_btn': 'تایید',
+    'home_search_search_clear_btn': 'حذف فیلتر ',
+    'vendor_home_history_flower_name': 'نام گل: ',
+    'vendor_home_history_customer_name': 'نام مشتری: ',
+    'vendor_home_history_bought_date': 'تاریخ خرید: ',
+    'vendor_home_history_price': 'قیمت: ',
+    'vendor_home_history_count_bought': 'تعداد خرید: ',
+    'profile_first_name': 'نام: ',
+    'profile_last_name': 'نام خانوادگی: ',
+    'profile_email': 'ایمیل: ',
+    'profile_sign_out_btn': 'خروج',
+    'customer_page_title': 'صفحه خانه خریدار',
+    'customer_page_refresh': 'بارگذاری مجدد',
+    'customer_page_home': 'خانه',
+    'customer_page_cart': 'سبد خرید',
+    'customer_page_search': 'جستوجو',
+    'customer_page_history': 'تاریخچه',
+    'customer_page_profile': 'مشخصات',
+    'customer_page_english_btn': 'English',
+    'customer_page_farsi_btn': 'فارسی',
+    'customer_page_about': 'در باره ما',
+    'customer_home_item_name': 'نام: ',
+    'customer_home_item_description': 'شرح: ',
+    'customer_home_item_vendor_name': 'اسم فروشنده: ',
+    'customer_home_item_color': 'رنگ: ',
+    'customer_home_item_add_cart_btn': 'اضافه به سبد',
+    'customer_home_item_price': 'قیمت: ',
+    'customer_home_item_count': 'تعداد: ',
+    'customer_home_item_out_of_stock': 'تمام شده',
+    'customer_home_item_count_buy': 'تعداد خرید',
+    'customer_home_item_total_price': 'قیمت کل: ',
+    'customer_home_item_purchase': 'خرید',
+    'customer_home_history_flower_name': 'نام گل: ',
+    'customer_home_history_vendor_name': 'نام فروشنده: ',
+    'customer_home_history_bought_date': 'تاریخ خرید: ',
+    'customer_home_history_price': 'قیمت: ',
+    'customer_home_history_count_bought': 'تعداد خرید: ',
+  };
+}
